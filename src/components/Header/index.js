b2eeb80a68d18495cea0d231297b15d952caa846
Artsiom Gonchar <tema.gon4ar@mail.ru>
2021-02-26 12:36:55 +0300

    feat: add fullscreen zoom

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,28 +2,51 @@ import React from 'react';
 import cl from 'classnames';
 import '../../css/common.css';
 import './style.css';
-import myLogoLight from './img/logo-light.png';
+import { FullscreenOutlined, FullscreenExitOutlined, SettingOutlined } from '@ant-design/icons';
 
 class Header extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = {};
+    this.state = {
+      onFullScreen : false
+    };
   };
   
+  onSetFullScreen = async () => {
+    if (document.fullscreenElement) {
+      document.exitFullscreen();
+    } else {
+      document.documentElement.requestFullscreen();
+    };
+
+    this.setState((state) => {
+      return{
+        onFullScreen : !state.onFullScreen,
+      }
+    });
+  };
+
   render() {
     return (
       <header>
         <div className={cl('container')}>
           <div className = {cl('header-blocks-wrap')}>
-            {/* <div className = {cl('header-logo')}>
-              <img src = {myLogoLight} />
-            </div>   */}
             <div className = {cl('header-name-game')}>
               <p>Memory Game</p>
             </div>  
             <div className = {cl('header-game-options')}>
-
+              <button
+                className={cl('btn', 'fullscreen-button')}
+                onClick = {this.onSetFullScreen}
+              >
+                {this.state.onFullScreen ? <FullscreenExitOutlined /> : <FullscreenOutlined />}
+              </button>
+              <button
+                className={cl('btn', 'setting-button')}
+              >
+                <SettingOutlined />
+              </button>
             </div>  
           </div>  
         </div>   
